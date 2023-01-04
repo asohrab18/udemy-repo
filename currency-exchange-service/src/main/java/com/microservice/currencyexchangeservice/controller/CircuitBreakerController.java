9b934b5373465bbc111dbc0a1fa34dec22699890
Sohrab Alam <sohrab.alam86@gmail.com>
2023-01-04 19:52:53 +0530

    @RateLimiter of Resilience4j applied

--- currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -2,12 +2,10 @@ package com.microservice.currencyexchangeservice.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 public class CircuitBreakerController {
@@ -15,15 +13,10 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
 	@GetMapping("/sample-api")
-	@CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
+	@RateLimiter(name = "default")
 	public String getSampleData() {
 		logger.info("sample-api call received.");
-		ResponseEntity<String> re = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
-		return re.getBody();
-	}
-
-	public String hardcodedResponse(Exception ex) {
-		return "fallback-response";
+		return "sample-response";
 	}
 
 }
