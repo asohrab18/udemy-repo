f9ea09d195c679d3749e14536cd747197049e6c5
Sohrab Alam <sohrab.alam86@gmail.com>
2023-01-04 20:01:58 +0530

    @Bulkhead of Resilience4j applied

--- currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -4,8 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 
 @RestController
 public class CircuitBreakerController {
@@ -13,7 +12,7 @@ public class CircuitBreakerController {
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
 	@GetMapping("/sample-api")
-	@RateLimiter(name = "default")
+	@Bulkhead(name = "default")
 	public String getSampleData() {
 		logger.info("sample-api call received.");
 		return "sample-response";
